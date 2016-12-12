c59a1dfc4cb7c6ad1caecd9087e3f00665e13bce
feizheng <1290657123@qq.com>
2016-12-12 21:42:23 +0800

    add react/react-dom

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -17,9 +17,6 @@ var webpackConfig = Object.assign({}, base)
 
 webpackConfig.devtool = '#source-map'
 webpackConfig.plugins = (webpackConfig.plugins || []).concat([
-  new webpack.IgnorePlugin(/^classnames$/),
-  new webpack.IgnorePlugin(/^react$/),
-  new webpack.IgnorePlugin(/^react-dom$/),
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: '"production"'
